768d132a3c412868e02c6abc3c091dc153438368
dewball345 <abhiraamkumar@gmail.com>
2020-03-15 16:29:53 -0700

    DONTW RORRY MAND
    lol

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1RkjyPdnWTLm4pEo2FYF7TujDFILk2DR6?alt=media&key=AIzaSyCpm06fzdXC2nW0h7joOHFh75KKinm8dwo'
+export_file_name = 'savannahAnimals.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['hippo', 'elephant', 'lion', 'gorilla', 'zebra']
 path = Path(__file__).parent
 
 app = Starlette()
